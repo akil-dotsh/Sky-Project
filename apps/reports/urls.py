9f7f8c74ce5286e5cc09693e382c9c15cefcaade
Akil Hossain <externaluse740@gmail.com>
2026-04-26 20:21:33 +0100

    Restore reports and profile picture updates

--- apps/reports/urls.py
+++ apps/reports/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns =[
     path("",views.admin_report,name="admin_report"),
-    path("generate/", views.report_generate, name="report_generate")
+    path("generate/", views.report_generate, name="report_generate"),
+    path("share/", views.share_report, name="share_report"),
+    path("user-reports/", views.user_reports, name="user_reports")
 ]
