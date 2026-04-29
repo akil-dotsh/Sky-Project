b3a636d5f9887a778b481a570577f410895bddb1
buckk1234-max <buckkaran1234@gmail.com>
2026-04-29 09:45:21 +0100

    Add files via upload
    Delete drafts works.

---
+++ apps/messaging/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = 'messaging' 
+
+urlpatterns = [
+    path('', views.inbox, name='inbox'),
+    path('sent/', views.sent, name='sent'),
+    path('draft/', views.draft, name='draft'),
+    path('messaging/', views.messaging, name='messaging'),
+    path("message/<int:pk>/", views.message_detail, name="detail"),
+    path("autosave/", views.autosave_draft, name="autosave_draft"),
+    path("message/<int:pk>/delete/", views.delete_message, name="delete"),
+    path("draft/<int:pk>/delete/", views.delete_draft, name="delete_draft")
+]
