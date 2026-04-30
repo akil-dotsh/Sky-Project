1817979ef510d0bd86d2ebbd1e682157d8a11472
buckk1234-max <buckkaran1234@gmail.com>
2026-04-30 11:34:48 +0100

    Update urls.py

--- apps/messaging/urls.py
+++ apps/messaging/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('draft/', views.draft, name='draft'),
     path('messaging/', views.messaging, name='messaging'),
     path("message/<int:pk>/", views.message_detail, name="detail"),
+    path("message/<int:pk>/", views.message_detail, name="message_detail"),
     path("autosave/", views.autosave_draft, name="autosave_draft"),
     path("message/<int:pk>/delete/", views.delete_message, name="delete"),
     path("draft/<int:pk>/delete/", views.delete_draft, name="delete_draft")
